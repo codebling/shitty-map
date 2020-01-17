2ff60656a31b475c0eab2c41bb02f84c7c507026
Code Bling <codeblingx@gmail.com>
2020-01-16 19:16:52 -0500

    Clarify descriptions and add a test case

--- test/index.js
+++ test/index.js
@@ -1,24 +1,26 @@
-const Map = require('../index');
+const ShittyMap = require('../index');
 const should = require('chai').should();
 const expect = require('chai').expect;
 
-describe('native Map backwards compatibility', function() {
-  it('should return undefined for an unknown key', function() {
-    const map = new Map();
-    map.set('here is', 'a value which will not be used');
-
-    expect(map.get(`this key isn't in the map`)).to.be.undefined;
+describe('native Map backwards-compatibility', function() {
+  describe('should have the same behaviour as native Map when we .get() an', () => {
+    it('unknown string key', function() {
+      expect(new ShittyMap().get('a non-existant key')).to.equal(new Map().get('another non-existant key'));
+    });
+    it('unknown Object key', function() {
+      expect(new ShittyMap().get({a: 'hi'})).to.equal(new Map().get({b: 'hello'}));
+    });
   });
 });
 
 describe('map.get', function() {
   it('should find a keymatch for an object which is a deep copy of a key', function() {
-    const map = new Map();
+    const smap = new ShittyMap();
     const key = { t: 1};
     const value = 'test';
-    map.set(key, value);
+    smap.set(key, value);
     const keyCopy = Object.assign({}, key);
     
-    map.get(keyCopy).should.equal(value);
+    smap.get(keyCopy).should.equal(value);
   });
 });
