a855781212d3641a06651962b3ed2b6837bc957a
Code Bling <codeblingx@gmail.com>
2020-01-16 19:03:47 -0500

    Rearrange code

--- test/index.js
+++ test/index.js
@@ -2,6 +2,15 @@ const Map = require('../index');
 const should = require('chai').should();
 const expect = require('chai').expect;
 
+describe('native Map backwards compatibility', function() {
+  it('should return undefined for an unknown key', function() {
+    const map = new Map();
+    map.set('here is', 'a value which will not be used');
+
+    expect(map.get(`this key isn't in the map`)).to.be.undefined;
+  });
+});
+
 describe('map.get', function() {
   it('should find a keymatch for an object which is a deep copy of a key', function() {
     const map = new Map();
@@ -12,14 +21,4 @@ describe('map.get', function() {
     
     map.get(keyCopy).should.equal(value);
   });
-});
-
-describe('native Map backwards compatibility', function() {
-  it('should return undefined for an unknown key', function() {
-    const map = new Map();
-    map.set('here is', 'a value which will not be used');
-
-    expect(map.get(`this key isn't in the map`)).to.be.undefined;
-  });
-
 });
