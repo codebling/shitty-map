04ba8b7f33f2db0e7167c8dbdca20336a77f8d50
Code Bling <codeblingx@gmail.com>
2020-01-16 19:43:43 -0500

    Consistently use arrow style for test blocks

--- test/index.js
+++ test/index.js
@@ -2,20 +2,20 @@ const ShittyMap = require('../index');
 const should = require('chai').should();
 const expect = require('chai').expect;
 
-describe('native Map backwards-compatibility', function() {
+describe('native Map backwards-compatibility', () => {
   describe('should have the same behaviour as native Map when we .get() an', () => {
-    it('unknown string key', function() {
+    it('unknown string key', () => {
       expect(new ShittyMap().get('a non-existent key')).to.equal(new Map().get('another non-existent key'));
     });
-    it('unknown Object key', function() {
+    it('unknown Object key', () => {
       expect(new ShittyMap().get({a: 'hi'})).to.equal(new Map().get({b: 'hello'}));
     });
   });
 });
 
-describe('expanded Object functionality', function() {
+describe('expanded Object functionality', () => {
   describe('should return value when we .get() using a ', () => {
-    it('copy of a simple Object key', function() {
+    it('copy of a simple Object key', () => {
       const smap = new ShittyMap();
       const key = { t: 1};
       const value = 'test';
@@ -26,7 +26,7 @@ describe('expanded Object functionality', function() {
       smap.get(keyCopy).should.equal(value);
     });
 
-    it('shallow copy of an Object key', function() {
+    it('shallow copy of an Object key', () => {
       const smap = new ShittyMap();
       const subobject = { bla: 5 }; 
       const key = { t: 1, grr: subobject };
@@ -38,7 +38,7 @@ describe('expanded Object functionality', function() {
       smap.get(keyCopy).should.equal(value);
     });
 
-    it('deep copy of an Object key', function() {
+    it('deep copy of an Object key', () => {
       const smap = new ShittyMap();
       const subobject = { bla: 5 }; 
       const key = { t: 1, grr: subobject };
